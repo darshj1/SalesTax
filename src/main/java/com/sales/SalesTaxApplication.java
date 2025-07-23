40fb95547a53a6d7e69d17c9a5af689e4176a453
Darsh Jain <yms.consultant2@mahindra.com>
2025-07-23 21:32:13 +0530

    Removing unused imports

--- src/main/java/com/sales/SalesTaxApplication.java
+++ src/main/java/com/sales/SalesTaxApplication.java
@@ -1,7 +1,5 @@
 package com.sales;
 
-import com.sales.model.Item;
-import com.sales.model.ReceiptItem;
 import com.sales.service.CartService;
 import com.sales.service.ReceiptService;
 import com.sales.service.TaxCalculatorService;
@@ -9,13 +7,9 @@ import com.sales.serviceImpl.CartServiceImpl;
 import com.sales.serviceImpl.ReceiptPrinter;
 import com.sales.serviceImpl.ReceiptServiceImpl;
 import com.sales.serviceImpl.TaxCalculatorServiceImpl;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-import java.util.List;
-import java.util.Scanner;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.sales")
